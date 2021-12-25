1b82c38b86011d5e28fe60e46fa3c810742cc291
JustSong <songquanpeng@foxmail.com>
2021-12-25 21:43:39 +0800

    :tada: update tui

--- public/static/main.js
+++ public/static/main.js
@@ -1,18 +1,24 @@
+let version = "v0.1.1";
 let promptElement = undefined;
 let inputElement = undefined;
 let connection = undefined;
-let prefix = ""
+let nickname = "";
 
 window.addEventListener('load', () => {
     inputElement = document.getElementById("input");
     promptElement = document.getElementById("prompt");
+    printBanner();
+
     let protocol = "ws://";
     if (location.protocol !== "http:") {
         protocol = "wss://"
     }
     connection = new WebSocket(protocol + document.location.host + "/ws");
+    if (!connection) {
+        print("", "system: failed to initialize connection.");
+    }
     connection.onopen = function (e) {
-        print("", "system: connected to server.");
+        // print("", "system: connected to server.");
     };
     connection.onclose = function (e) {
         print("", "system: connection closed.");
@@ -22,55 +28,74 @@ window.addEventListener('load', () => {
         print("", `system: error happened.`);
     };
     connection.onmessage = function (e) {
-        print("", e.data);
+        if (nickname === "" || !e.data.startsWith(nickname)) {
+            print("", e.data, false);
+        }
     }
 })
+
+function printBanner() {
+    print("", `Go Text ${version} on ${navigator.userAgent}`);
+    print("", `Type "help", "copyright", "repository" or "license" for more information.`);
+}
 
 function processInput(input) {
     if (input === "") input = " ";
     print(input, "");
-    if (input.startsWith("!")) {
-        let cmd = input.split(' ')[0];
-        switch (cmd) {
-            case "!help":
-                print("", `!help: print help information.`)
-                print("", `!prefix: set prefix.`)
-                break
-            case "!prefix":
-                prefix = input.slice(7).trim();
-                print("", `system: prefix set to ${prefix}.`)
-                break
-            default:
-                print("", `system: command "${cmd}" not found.`)
-        }
-    } else {
-        send(input);
+    let cmd = input.split(' ')[0];
+    switch (cmd) {
+        case "help":
+            print("", `help: print help information.`)
+            print("", `nickname: set nickname.`)
+            print("", `copyright: print copyright information.`)
+            print("", `repository: show repository url.`)
+            print("", `license: print license information.`)
+            break
+        case "copyright":
+            print("", `Copyright (c) 2021 JustSong.`)
+            print("", "All Rights Reserved.")
+            break
+        case "repository":
+            print("", `See <a href="https://github.com/songquanpeng/go-text">https://github.com/songquanpeng/go-text</a>`)
+            break
+        case "license":
+            print("", `See <a href="https://github.com/songquanpeng/go-text/blob/master/LICENSE">https://github.com/songquanpeng/go-text/blob/master/LICENSE</a>`)
+            break
+        case "nickname":
+            nickname = input.slice(8).trim();
+            print("", `system: nickname set to ${nickname}.`)
+            break
+        default:
+            send(input);
     }
+
 }
 
 
 function send(message) {
-    if (prefix !== "") {
-        connection.send(`${prefix}: ${message}`);
+    if (nickname !== "") {
+        connection.send(`${nickname}: ${message}`);
     } else {
         connection.send(`${message}`);
     }
 }
 
 
-function print(input, output) {
-    inputElement.value = "";
+function print(input, output, clearInput = true) {
+    if (clearInput) {
+        inputElement.value = "";
+    }
     if (input !== "") {
         let inputElement = `<div class="line">
-        <span class="prompt">&gt;</span>
+        <span class="prompt">&gt;&gt;&gt;</span>
         <span>${input}</span>
     </div>`;
         promptElement.insertAdjacentHTML('beforebegin', inputElement);
     }
+    // <!--        <span class="prompt">&lt;</span>-->
+    // <!--        <span class="prompt">&nbsp;</span>-->
     if (output !== "") {
         let outputElement = `<div class="line">
-<!--        <span class="prompt">&lt;</span>-->
-        <span class="prompt">&nbsp;</span>
         <span>${output}</span>
     </div>`;
         promptElement.insertAdjacentHTML('beforebegin', outputElement);
